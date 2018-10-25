5f413c28fe3386b34755c9c06825bdc2b37b0350
Casey Eich <cseich11@gmail.com>
2018-10-25 17:17:50 -0400

    InventoryItem model and controller created

---
+++ app/Http/Controllers/InventoryItemsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InventoryItem;
+use DB;
+
+class InventoryItemsController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
