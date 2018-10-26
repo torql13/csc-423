3eeacf9adee154d09187d8d50e77694fe5a785ac
Torrin Lisi <torrinlisi@gmail.com>
2018-10-26 17:24:17 -0400

    views

--- resources/views/Vendor/viewVendor.blade.php
+++ resources/views/Vendor/viewVendor.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
+    <div class="container mt-4">
+        <div class="well">
+            <div class="row">
                 <h3 class="mt-3">Information for {{ $indVendor->VendorName }}</h3>
+            </div>
+            <div class="row">
                 <p><strong>Store Code: </strong>{{ $indVendor->VendorCode }}</p>
+            </div>
+            <div class="row">
                 <p><strong>Address: </strong>{{ $indVendor->Address }}, {{ $indVendor->State }}, {{ $indVendor->ZIP }}</p>
+            </div>
+            <div class="row">
                 <p><strong>Phone: </strong>{{ $indVendor->Phone }}</p>
+            </div>
+            <div class="row">
                 <p><strong>Contact Person: </strong>{{ $indVendor->ContactPersonName }}</p>
-           </div>
+            </div>
         </div>
     </div>
 @stop
