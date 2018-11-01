98835fb722d6222a7511376fd8a1eb8ca33301f6
jakethurnau <jthurnau98@gmail.com>
2018-10-31 20:58:47 -0400

    Update app/StoreLocation.php

--- app/StoreLocation.php
+++ app/StoreLocation.php
@@ -6,17 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class StoreLocation extends Model
 {
-     //define name of table associated with the model
-     protected $table = 'retail_store';
-
-     //define name of primary key for model's associated table
-     protected $primaryKey = 'StoreId';
- 
-     /**
-      * gets the order details that belong to an order
-      */
-     public function stores()
-     {
-         return $this->hasMany('App\StoreLocation', 'StoreId', 'StoreId');
-     }
+     
 }
