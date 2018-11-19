ee0479815c277e8f0d22f4269da93b305bfc50a5
Torrin Lisi <torrinlisi@gmail.com>
2018-11-19 17:54:17 -0500

    tweak

--- resources/views/Order/index.blade.php
+++ resources/views/Order/index.blade.php
@@ -27,7 +27,7 @@
                                 <a href="/order/addDetailsExistingOrder/{{$order->OrderId}}"> <i class="material-icons" style="font-size:36px;color:green;" title="Add to this Order">add_box</i></a>
                                 <a href="/order/processDelivery/{{$order->OrderId}}" onclick="return confirm('Process this delivery?');"><i class="material-icons" style="font-size:36px;color:blue;" title="Process Delivery">airport_shuttle</i></a>
                             @elseif($order->Status === 'Delivered')
-                                <a href="/order/processDelivery/{{$order->OrderId}}" onclick="return confirm('Process this Return?');"><i class="material-icons" style="font-size:36px;color:red;" title="Process Return">keyboard_return</i></a>
+                                <a href="#" onclick="return confirm('Process this Return?');"><i class="material-icons" style="font-size:36px;color:red;" title="Process Return">keyboard_return</i></a>
                             @elseif($order->Status === 'Returned')
                                 No Actions
                             @endif
