ac33485a43073198907a8f7dc601ea9e8299fcbe
jakethurnau <jthurnau98@gmail.com>
2018-11-18 21:18:20 -0500

    finihsed up icons

--- resources/views/Order/index.blade.php
+++ resources/views/Order/index.blade.php
@@ -15,16 +15,21 @@
                             <th>OrderId</th>
                             <th>DateTimeOfOrder</th>
                             <th>Status</th>
-                            <th>Add Items to Order</th>
-                            <th>Delivery</th>
+                            <th>Actions</th>
                         </tr>
                         @foreach ($orders as $order)
                         <tr>
                             <td>{{$order->OrderId}}</td>
                             <td>{{$order->DateTimeOfOrder}}</td>
                             <td>{{$order->Status}}</td>
-                            <td><a href="/order/addDetailsExistingOrder/{{$order->OrderId}}"> <i class="material-icons" style="font-size:36px;color:green;" title="Add to this Order">add_box</i></a></td>
-                            <td><a href="/order/processDelivery/{{$order->OrderId}}" onclick="return confirm('Process this delivery?');">Process Delivery</a></td>
+                            <td>
+                            @if($order->Status === 'Delivered')
+                                <a href="/order/processReturn/{{$order->OrderId}}"> <i class="material-icons" style="font-size:36px;color:red;" title="Process Return">keyboard_return</i></a>
+                            @elseif($order->Status === 'Pending')
+                                <a href="/order/addDetailsExistingOrder/{{$order->OrderId}}"> <i class="material-icons" style="font-size:36px;color:green;" title="Add to this Order">add_box</i></a>
+                                <a href="/order/processDelivery/{{$order->OrderId}}" onclick="return confirm('Process this delivery?');"><i class="material-icons" style="font-size:36px;color:blue;" title="Process Delivery">airport_shuttle</i></a>
+                            @endif
+                           </td>
                         </tr>
                         @endforeach
                     </table>
