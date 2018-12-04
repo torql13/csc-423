9a4020ff6d98d442bef25d2606c57974b92233d2
jakethurnau <jthurnau98@gmail.com>
2018-12-03 21:43:39 -0500

    added icon for overstock function

--- resources/views/StoreLocation/storeIndex.blade.php
+++ resources/views/StoreLocation/storeIndex.blade.php
@@ -62,7 +62,7 @@
                                 <a href="/storeLocations/editLocation/{{ $storeLocation->StoreId }}"> <i class="material-icons" style="font-size:36px;color:blue;" title="Edit">edit</i></a>
                                 <a href="/storeLocations/deleteLocation/{{ $storeLocation->StoreId }}" onclick="return confirm('Are you sure?');"> <i class="material-icons" style="font-size:36px;color:red;" title="Delete">delete</i></a>
                                 <a href="/report/inventoryReport/{{$storeLocation->StoreId}}"> <i class="material-icons" style="font-size:36px;color:orange;" title="Inventory">shopping_cart</i> </a>
-                                <a href="/report/overstockedInventory/{{$storeLocation->StoreId}}"> overstock report </a>
+                                <a href="/report/overstockedInventory/{{$storeLocation->StoreId}}"> <i class="material-icons" style="font-size:36px;color:black;" title="Overstock">reorder</i> </a>
                             </td>
                         </tr>
                         @endforeach
