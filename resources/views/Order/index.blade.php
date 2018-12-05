0504469bf9b6ce73ff2532414aaf84d48e89b008
Torrin Lisi <torrinlisi@gmail.com>
2018-12-05 17:31:39 -0500

    Remove un needed code after change

--- resources/views/Order/index.blade.php
+++ resources/views/Order/index.blade.php
@@ -71,10 +71,8 @@
                             @elseif($order->Status === 'Delivered')
                                 <a href="/order/viewOrder/{{$order->OrderId}}"> <i class="material-icons" style="font-size:36px;color:green;" title="View">visibility</i></a>
                                 <a href="/order/processReturn/{{$order->OrderId}}" onclick="return confirm('Process this Return?');"><i class="material-icons" style="font-size:36px;color:red;" title="Process Return">keyboard_return</i></a>
-                            @elseif($order->Status === 'Returned')
-                                <a href="/order/viewOrder/{{$order->OrderId}}"> <i class="material-icons" style="font-size:36px;color:green;" title="View">visibility</i></a>
-                            @endif
                            </td>
+                           @endif
                         </tr>
                         @endforeach
                     </table>
